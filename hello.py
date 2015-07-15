a58cf31f69021a9f94fb96e7981f4315e1e0016a
javiersoriano <javisd23@gmail.com>
2015-07-15 21:42:01 +0900

    added counter feature

--- hello.py
+++ hello.py
@@ -3,25 +3,29 @@ import uuid
 from flask import Flask
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
-BLUE = "#0099FF"
+BLUE = "#AA99FF"
 GREEN = "#33CC33"
+counter = 0
 
 COLOR = BLUE
 
 @app.route('/')
 def hello():
-
+	global counter
+	counter = counter + 1
 	return """
 	<html>
 	<body bgcolor="{}">
 
 	<center><h1><font color="white">Hi, I'm GUID:<br/>
+	{}<br/>
 	{}
 	</center>
 
 	</body>
 	</html>
-	""".format(COLOR,my_uuid)
+	""".format(COLOR,my_uuid,counter)
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
